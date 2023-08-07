a37368f8acd8b5c876b2dc7b1435fcae55bef64c
Your Name <torik.09m@gmail.com>
2023-08-07 18:03:35 +0300

    'Mission accomplished'

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,15 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld", HelloWorld.class);
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld", HelloWorld.class);
         System.out.println(bean.getMessage());
+
+        Cat bean2 = (Cat) applicationContext.getBean("cat", Cat.class);
+        Cat bean3 = (Cat) applicationContext.getBean("cat", Cat.class);
+        System.out.println(bean2.getMassage());
+
+        System.out.println((bean == bean1));
+        System.out.println((bean2 == bean3));
     }
 }
